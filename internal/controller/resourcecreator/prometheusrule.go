b3ec6c094cc5163bbe0575406ccf8b4b3d52eb24
Sindre Rødseth Hansen <--global>
2025-10-30 11:58:13 +0100

    fix rule

--- internal/controller/resourcecreator/prometheusrule.go
+++ internal/controller/resourcecreator/prometheusrule.go
@@ -34,7 +34,7 @@ func CreatePrometheusRuleSpec(postgres *data_nais_io_v1.Postgres, pgClusterName 
 					{
 						Alert: "PostgresMemoryUsageHigh",
 						Expr: intstrFromString(fmt.Sprintf(
-							"(avg(container_memory_usage_bytes{container=\"postgres\", namespace=\"%s\", pod=~\"%s-[0-9]\"}) by (pod) / avg(kube_pod_container_resource_limits{container=\"postgres\", resource=\"memory\", namespace=\"%s\", pod=~\"%s-[0-9]\"}) by (pod)) > 0.00009", pgNamespace, pgClusterName, pgNamespace, pgClusterName)),
+							"(avg(container_memory_usage_bytes{container=\"postgres\", namespace=\"%s\", pod=~\"%s-[0-9]\"}) by (pod) / avg(kube_pod_container_resource_limits{container=\"postgres\", resource=\"memory\", namespace=\"%s\", pod=~\"%s-[0-9]\"}) by (pod)) > 0.9", pgNamespace, pgClusterName, pgNamespace, pgClusterName)),
 						For: ptr.To(monitoring_v1.Duration("10s")),
 						Labels: map[string]string{
 							"severity": "warning",
