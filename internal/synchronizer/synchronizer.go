a8df5600022c1708567e5d71bfbc668f3f476227
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2026-01-23 14:59:39 +0100

    intellij

--- internal/synchronizer/synchronizer.go
+++ internal/synchronizer/synchronizer.go
@@ -66,8 +66,8 @@ func findRelevantListTypes[T object.NaisObject, P any](r reconciler.Reconciler[T
 			}
 			for _, relevantGvk := range relevantGvks {
 				if relevantGvk.Group == groupVersionKind.Group &&
-						relevantGvk.Version == groupVersionKind.Version &&
-						fmt.Sprintf("%sList", relevantGvk.Kind) == groupVersionKind.Kind {
+					relevantGvk.Version == groupVersionKind.Version &&
+					fmt.Sprintf("%sList", relevantGvk.Kind) == groupVersionKind.Kind {
 					listTypes[groupVersionKind] = r
 				}
 			}
@@ -264,14 +264,14 @@ func (s *Synchronizer[T, P]) SetupWithManager(mgr ctrl.Manager) error {
 	builder := ctrl.NewControllerManagedBy(mgr).
 		For(s.reconciler.New()).
 		WithOptions(opts).
-			WithEventFilter(predicate.Or(
-				GenerationChangedPredicate{
-					Scheme:   mgr.GetScheme(),
-					MainKind: findKind(s.reconciler.New(), mgr.GetScheme()),
-				},
-				predicate.AnnotationChangedPredicate{},
-				predicate.LabelChangedPredicate{},
-			)).
+		WithEventFilter(predicate.Or(
+			GenerationChangedPredicate{
+				Scheme:   mgr.GetScheme(),
+				MainKind: findKind(s.reconciler.New(), mgr.GetScheme()),
+			},
+			predicate.AnnotationChangedPredicate{},
+			predicate.LabelChangedPredicate{},
+		)).
 		Named(s.reconciler.Name())
 	for _, t := range s.reconciler.OwnedTypes() {
 		builder = builder.Owns(t)
@@ -399,11 +399,11 @@ func findKind(obj client.Object, scheme *runtime.Scheme) string {
 
 func isNil(arg any) bool {
 	if v := reflect.ValueOf(arg); !v.IsValid() || ((v.Kind() == reflect.Ptr ||
-			v.Kind() == reflect.Interface ||
-			v.Kind() == reflect.Slice ||
-			v.Kind() == reflect.Map ||
-			v.Kind() == reflect.Chan ||
-			v.Kind() == reflect.Func) && v.IsNil()) {
+		v.Kind() == reflect.Interface ||
+		v.Kind() == reflect.Slice ||
+		v.Kind() == reflect.Map ||
+		v.Kind() == reflect.Chan ||
+		v.Kind() == reflect.Func) && v.IsNil()) {
 		return true
 	}
 	return false
