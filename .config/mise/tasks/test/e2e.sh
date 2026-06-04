098fee1a2660b98fcfda1ab324697dbf2e603e9f
Morten Lied Johansen <morten.lied.johansen@nav.no>
2026-06-04 12:29:04 +0200

    Wait for tilt deployment to be ready before running e2e tests

--- .config/mise/tasks/test/e2e.sh
+++ .config/mise/tasks/test/e2e.sh
@@ -2,4 +2,5 @@
 # [MISE] description="Run e2e tests with chainsaw (requires running mise run dev:tilt in a different terminal)"
 set -euo pipefail
 
+tilt wait --for=condition=Ready "uiresource/pgrator" --timeout 2m
 chainsaw test --kube-context kind-pgrator --test-dir tests/e2e/ --config .chainsaw.yaml
