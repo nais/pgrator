459c55f2573eadf307572573226d2ae4cbc2d148
Morten Lied Johansen <morten.lied.johansen@nav.no>
2026-06-09 13:43:42 +0200

    build doesn't need to depend on fmt and check

--- .config/mise/tasks/build.sh
+++ .config/mise/tasks/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #MISE description="Build manager binary"
-#MISE depends=["fmt", "check", "generate"]
+#MISE depends=["generate"]
 set -euo pipefail
 
 go build -o bin/manager cmd/main.go
